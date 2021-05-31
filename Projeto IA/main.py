b2bed040fb4d87d8e5494e6a39b5d94ec88e960e
WalmerAlmeida <43154142+WalmerAlmeida@users.noreply.github.com>
2021-05-31 18:05:33 -0300

    Add files via upload

--- Projeto IA/main.py
+++ Projeto IA/main.py
@@ -5,16 +5,19 @@ import sys
 engine = knowledge_engine.engine(__file__)
 
 
-def bc_test():
+def bc_test(bola, turbidez, coliformes_termotolerantes, solidos_dissolvidos_totais, cloro_residual):
     engine.reset()      # Allows us to run tests multiple times.
     
     
     engine.activate('water_reuse_rules')
-    
+
+    engine.assert_('facts', 'pH', (bola,))    
 
     print("doing proof")
     try:
-        with engine.prove_goal('water_reuse_rules.qual_classe($classe, $turbidez, $coliformes_termotolerantes, $solidos_dissolvidos_totais, $cloro_residual)', turbidez = 4, coliformes_termotolerantes = 100, solidos_dissolvidos_totais = 100, cloro_residual = 1) as gen:
+        with engine.prove_goal(
+            'water_reuse_rules.qual_classe($classe, $turbidez, $coliformes_termotolerantes, $solidos_dissolvidos_totais, $cloro_residual)', \
+                turbidez=turbidez, coliformes_termotolerantes=coliformes_termotolerantes, solidos_dissolvidos_totais=solidos_dissolvidos_totais, cloro_residual=cloro_residual) as gen:
             for vars, plan in gen:
                 print("A classe da água é: %s" % (vars['classe']))
     except Exception:
@@ -27,4 +30,6 @@ def bc_test():
     print("done")
     engine.print_stats()
 
-bc_test()
+bc_test(True, 4, 100, 100, 1)
+bc_test(False, 4, 100, 100, 1)
+bc_test(True, 4, 100, 100, 1)
