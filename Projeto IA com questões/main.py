c677bc11c734cd1706d501fd92293914615ad241
WalmerAlmeida <43154142+WalmerAlmeida@users.noreply.github.com>
2021-06-01 22:41:17 -0300

    Add files via upload

--- Projeto IA com questões/main.py
+++ Projeto IA com questões/main.py
@@ -1,6 +1,7 @@
 from pyke import knowledge_engine, krb_traceback, goal, ask_tty
 
 import sys
+import numpy as np
 
 engine = knowledge_engine.engine(__file__)
 
@@ -10,24 +11,20 @@ engine.ask_module = ask_tty
 def bc_test():
     engine.reset()      # Allows us to run tests multiple times.
     
+    comentarios = []
+
     print("\nInsira os dados da água que deseja reutilizar\n")
     
     engine.activate('water_reuse_rules')
 
-    print("doing proof")
-    try:
-        with engine.prove_goal(
-            'water_reuse_rules.qual_classe($classe)') as gen:
-            for vars, plan in gen:
-                print("A classe da água é: %s" % (vars['classe']))
-    except Exception:
-        # This converts stack frames of generated python functions back to the
-        # .krb file.
-        krb_traceback.print_exc()
-        sys.exit(1)
-    
-    print()
-    print("done")
-    #engine.print_stats()
+    with engine.prove_goal(
+        'water_reuse_rules.comentarios($comentarios)') as gen:
+        for vars, plan in gen:
+            comentarios.append(vars['comentarios'])
+
+    print("\nComentários sobre a água que deseja fazer reúso:\n")
+    for i in range(len(comentarios)):
+        print("-----------------------")
+        print(comentarios[i])
 
 bc_test()
