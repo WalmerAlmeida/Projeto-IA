b567ecb3ec2b8c706690de876a7c39f2399e3d26
WalmerAlmeida <43154142+WalmerAlmeida@users.noreply.github.com>
2021-06-02 12:17:18 -0300

    Add files via upload

--- Projeto IA/main.py
+++ Projeto IA/main.py
@@ -1,33 +1,30 @@
-from pyke import knowledge_engine, krb_traceback, goal
+from pyke import knowledge_engine, krb_traceback, goal, ask_tty
 
 import sys
+import numpy as np
 
 engine = knowledge_engine.engine(__file__)
 
+engine.ask_module = ask_tty
 
-def bc_test(bola, turbidez, coliformes_termotolerantes, solidos_dissolvidos_totais, cloro_residual):
+
+def bc_test():
     engine.reset()      # Allows us to run tests multiple times.
     
+    comentarios = []
+
+    print("\nInsira os dados da água que deseja reutilizar\n")
     
     engine.activate('water_reuse_rules')
 
-    engine.assert_('facts', 'pH', (bola,))    
+    with engine.prove_goal(
+        'water_reuse_rules.comentarios($comentarios)') as gen:
+        for vars, plan in gen:
+            comentarios.append(vars['comentarios'])
 
-    print("doing proof")
-    try:
-        with engine.prove_goal(
-            'water_reuse_rules.qual_classe($classe, $turbidez, $coliformes_termotolerantes, $solidos_dissolvidos_totais, $cloro_residual)', \
-                turbidez=turbidez, coliformes_termotolerantes=coliformes_termotolerantes, solidos_dissolvidos_totais=solidos_dissolvidos_totais, cloro_residual=cloro_residual) as gen:
-            for vars, plan in gen:
-                print("A classe da água é: %s" % (vars['classe']))
-    except Exception:
-        # This converts stack frames of generated python functions back to the
-        # .krb file.
-        krb_traceback.print_exc()
-        sys.exit(1)
-    
-    print()
-    print("done")
-    engine.print_stats()
+    print("\nComentários sobre a água que deseja fazer reúso:\n")
+    for i in range(len(comentarios)):
+        print("-----------------------")
+        print(comentarios[i])
 
-bc_test(True, 4, 100, 100, 1)
+bc_test()
